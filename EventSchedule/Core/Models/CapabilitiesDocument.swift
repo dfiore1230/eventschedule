b238f849d9d7321da1c218634707d74251c1c8ae
dfiore1230 <dfiore1230@users.noreply.github.com>
2025-12-07 11:38:42 -0500

    Handle legacy branding endpoint key

--- EventSchedule/Core/Models/CapabilitiesDocument.swift
+++ EventSchedule/Core/Models/CapabilitiesDocument.swift
@@ -23,11 +23,27 @@ struct CapabilitiesDocument: Codable {
     enum CodingKeys: String, CodingKey {
         case apiBaseURL = "api_base_url"
         case auth
-        case brandingEndpoint = "brandingendpoint"
+        case brandingEndpoint = "branding_endpoint"
+        case legacyBrandingEndpoint = "brandingendpoint"
         case features
         case versions
         case minAppVersion = "min_app_version"
         case rateLimits = "rate_limits"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        apiBaseURL = try container.decode(URL.self, forKey: .apiBaseURL)
+        auth = try container.decode(AuthConfig.self, forKey: .auth)
+        if let endpoint = try container.decodeIfPresent(URL.self, forKey: .brandingEndpoint) {
+            brandingEndpoint = endpoint
+        } else {
+            brandingEndpoint = try container.decode(URL.self, forKey: .legacyBrandingEndpoint)
+        }
+        features = try container.decode([String: Bool].self, forKey: .features)
+        versions = try container.decodeIfPresent([String: String].self, forKey: .versions)
+        minAppVersion = try container.decodeIfPresent(String.self, forKey: .minAppVersion)
+        rateLimits = try container.decodeIfPresent([String: Int].self, forKey: .rateLimits)
     }
 }
 
