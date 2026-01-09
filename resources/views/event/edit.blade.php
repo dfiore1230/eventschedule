4e340b27ff784ca50086959cbb585f350598b1bd
dfiore1230 <dfiore1230@users.noreply.github.com>
2026-01-09 16:35:19 -0500

    Rename notifications button label

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -176,7 +176,7 @@
   <div class="flex justify-end mb-4">
     <a href="{{ route('event.notifications', ['subdomain' => $subdomain, 'hash' => \App\Utils\UrlUtils::encodeId($event->id)]) }}"
        class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      {{ __('messages.notification_settings') ?? 'Notification settings' }}
+      Event Specific Notifications
     </a>
   </div>
   @endif
